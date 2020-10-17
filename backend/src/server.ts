42b4300176472b3ef00d492ec1be85236b5dfee7
Marcus Couto <markitovc@gmail.com>
2020-10-16 23:21:34 -0300

    create server andconfig typescript

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+import './database/connection'
+
+const app = express();
+
+app.use(express.json())
+
+
+app.post("/orphanages", (req, res) => {
+  return res.json({ message: "bla.... world" });
+});
+
+app.listen(3333)
